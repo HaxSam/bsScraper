e99bb05c30055e66b476ac3a2cd17d569e20fb13
Haysam <haysam2002@gmail.com>
2020-09-12 16:15:30 +0200

    Tasting to get the Links from one Session

--- tasks/bsSerien.js
+++ tasks/bsSerien.js
@@ -1,1 +1,12 @@
-const https = require("https");
+const bent = require("bent");
+const cheerio = require("cheerio");
+
+let bs = bent("https://bs.to");
+
+(async (url) => {
+	let res = await bs(url);
+	let $ = cheerio.load(await res.text());
+
+	let links = $(`li[class^='e'] > a[href*='${url.match(/\/\d+\//)[0]}'][href$='/de']`).toArray();
+	for (let a of links) console.log(a.attribs.href);
+})("/serie/Die-Simpsons/1/1-Es-Weihnachtet-Schwer/de");
